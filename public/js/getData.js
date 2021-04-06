e087fb56a0edcde02b4766fa38ca07099de6522c
wmordy <mwadii30@gmail.com>
2021-04-06 15:36:18 +0100

    fix bugs 7

--- public/js/getData.js
+++ public/js/getData.js
@@ -2,8 +2,7 @@ var getDouaa = async () => {
     let resp = await fetch("/douaa")
     let data2 = await resp.json()
     
-    let douaaSection = document.getElementById("douaa-title")
-    douaaSection.innerText = data.day
+   
     let douaaBody = document.getElementById("douaa-body")
     douaaBody.innerText = data.douaa[0]
 }
@@ -63,7 +62,38 @@ var citiesAr = ["مكة المكرمة",
     "سكاكا"
 
 ]
-
+var numDay = [
+    "الأول",
+    "الثاني",
+    "الثالث",
+    "الربع",
+    "الخامس",
+    "السادس" ,
+    "السابع",
+    "الثامن",
+    "التاسع",
+    "العاشر",
+    "الحادي عشر",
+    "الثاني عشر",
+    "الثالث عشر",
+    "الرابع عشر",
+    "الخامس عشر",
+    "السادس عشر",
+    "السابع عشر",
+    "الثامن عشر",
+    "التاسع عشر",
+    "عشرون",
+    "الحادي والعشرون",
+    "الثاني والعشرون",
+    "الثالث و العشرون",
+    "الرابع و العشرون",
+    "الخامس و العشرون",
+    "السادس و العشرون",
+    "السابع و العشرون",
+    "الثامن و العشرون",
+    "التاسع و العشرون",
+    "الثلاثون "
+]
 var getDate = async () => {
     for(var i = 0 ; i < cities.length ; i++){
         let apiMsg = await fetch("https://api.aladhan.com/v1/timingsByAddress?address="+cities[i])
@@ -77,6 +107,9 @@ var getDate = async () => {
         document.getElementById("arab-mounth-en").innerText = hijri.month.en
         document.getElementById("arab-year").innerText = hijri.year
     
+        let douaaSection = document.getElementById("douaa-title")
+        douaaSection.innerText = numDay[Number.parseInt(hijri.day)-1]
+
         document.getElementById("en-day").innerText = gregorian.day
         document.getElementById("en-mounth").innerText = arMonths[Number.parseInt(hijri.month.number)+1] 
         document.getElementById("en-mounth-en").innerText = gregorian.month.en
